6e402e5a1176d71ac6167c0b3777660f1c4cb860
Owais Ahmad <91786886+owaisahmad315@users.noreply.github.com>
2024-10-02 20:18:47 +0500

    connected database and server is running

---
+++ server/server.js
@@ -0,0 +1,40 @@
+
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+
+const express = require('express')
+const mongoose = require('mongoose')
+
+//create a database connection -> u can also
+//create a separate file for this and then import/use that file here
+
+
+
+mongoose.connect('mongodb+srv://owaisahmad014:Mymern%40321@cluster0.qybw4.mongodb.net/')
+.then(() => console.log('Mongo Database Connected'))
+.catch((error) => console.log('Connection error:', error));
+
+const app = express()
+
+const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST", "DELETE", "PUT"],
+      allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "Cache-Control",
+        "Expires",
+        "Pragma",
+      ],
+      credentials: true,
+    })
+  );    
+
+app.use(cookieParser());
+app.use(express.json());
+
+  app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
