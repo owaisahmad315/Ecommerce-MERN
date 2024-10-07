6587243c5d62dca0148a13d43ebe9e2b16f04865
Owais Ahmad <91786886+owaisahmad315@users.noreply.github.com>
2024-10-07 11:00:06 +0500

    worked on routes protection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,31 @@
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
-import AuthLayout from './components/auth/layout'
-import AuthLogin from './pages/auth/login'
-import AuthRegister from './pages/auth/register'
+import AuthLayout from './components/auth/layout';
+import AuthLogin from './pages/auth/login';
+import AuthRegister from './pages/auth/register';
 
 import AdminLayout from "./components/admin-view/layout";
 import AdminDashboard from "./pages/admin-view/dashboard";
 import AdminProducts from "./pages/admin-view/products";
 import AdminOrders from "./pages/admin-view/orders";
 
-import ShoppingLayout from "./components/shopping-view/layout"
+import ShoppingLayout from "./components/shopping-view/layout";
 import NotFound from './pages/not-found';
-import ShoppingCheckout from './pages/shopping-view/checkout'
-import ShoppingHome from './pages/shopping-view/home'
-import ShoppingListing from './pages/shopping-view/listing'
-import ShoppingAccount from './pages/shopping-view/account'
+import ShoppingCheckout from './pages/shopping-view/checkout';
+import ShoppingHome from './pages/shopping-view/home';
+import ShoppingListing from './pages/shopping-view/listing';
+import ShoppingAccount from './pages/shopping-view/account';
+
+import CheckAuth from './components/common/check-auth';
 
 
 
 function App() {
+  
 
+  const isAuthenticated = false;
+  const user = null;
   return (
     <>
       <div  className="flex flex-col overflow-hidden bg-white">
@@ -28,19 +33,31 @@ function App() {
 
 
         <Routes>
-
-          <Route path='/auth' element={<AuthLayout/>}>
+          <Route path='/auth' element={
+            <CheckAuth isAuthenticated={isAuthenticated} user ={ user}>
+             <AuthLayout/>
+            </CheckAuth >
+          }>
+            
             <Route path='login' element={<AuthLogin/>}/>
             <Route path='register' element={<AuthRegister/>}/>
           </Route>
 
-          <Route path='/admin' element={<AdminLayout />}>
+          <Route path='/admin' element={
+            <CheckAuth isAuthenticated={isAuthenticated} user ={ user}>
+             <AdminLayout/>
+           </CheckAuth >
+          }>
             <Route path='products' element={<AdminProducts/>}/>
             <Route path='dashboard' element={<AdminDashboard/>}/>
             <Route path='orders' element={<AdminOrders/>}/>
           </Route>
 
-          <Route path='/shop' element={<ShoppingLayout />}>
+          <Route path='/shop' element={
+            <CheckAuth isAuthenticated={isAuthenticated} user ={ user}>
+             <ShoppingLayout />          
+           </CheckAuth >
+          }>
             <Route path='*' element={<NotFound/>}/>
             <Route path='checkout' element={<ShoppingCheckout/>}/>
             <Route path='home' element={<ShoppingHome/>}/>
@@ -55,4 +72,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
