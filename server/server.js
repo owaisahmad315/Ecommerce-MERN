8bde3306b2544be5b012f60014489655edb4faaa
Owais Ahmad <91786886+owaisahmad315@users.noreply.github.com>
2024-10-12 10:32:15 +0500

    server side basic setup done

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const cors = require("cors");
 const express = require('express')
 const mongoose = require('mongoose')
 
+const authRouter = require('./routes/auth/auth-routes')
+
 //create a database connection -> u can also
 //create a separate file for this and then import/use that file here
 
@@ -36,5 +38,6 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
+app.use('/api/auth',authRouter)
 
   app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
